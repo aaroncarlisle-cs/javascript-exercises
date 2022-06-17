3e67474cdce534d08dc33685413fcb2b342d14a9
Aaron Carlisle <aaroncarlisle.cs@gmail.com>
2022-06-17 11:28:16 -0600

    Fix bug if args are inbetween -1 and 0.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(num1, num2) {
     if (typeof num1 === typeof num2) {
-        if (num1 > -1 && num2 > -1) {
+        if (num1 >= 0 && num2 >= 0) {
             // Formula for sum of all natural numbers inclusive
             // ((a + b)*(b - a + 1)) / 2 
             if (num1 < num2) {
