d721a4ed16e57615c6e8869ebeb4b3d09cde1d17
Aaron Carlisle <aaroncarlisle.cs@gmail.com>
2022-06-17 11:25:23 -0600

    Use formula for sum of all natural numbers inclusive.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof num1 === typeof num2) {
+        if (num1 > -1 && num2 > -1) {
+            // Formula for sum of all natural numbers inclusive
+            // ((a + b)*(b - a + 1)) / 2 
+            if (num1 < num2) {
+                return ((num1 + num2) * (num2 - num1 + 1)) / 2;     
+            }
+            else if (num1 > num2) {
+                return ((num2 + num1) * (num1 - num2 + 1)) / 2;
+            }
+            else {
+                return num1 + num2;
+            }
+        }
+    }
+    return "ERROR";
 };
 
 // Do not edit below this line
