a12ec52957bb2d693a29705aa23c2383f5cbea8a
Aaron Carlisle <aaroncarlisle.cs@gmail.com>
2022-06-23 13:40:05 -0600

    Enable and confirm all tests pass.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(index) {
+let a = 0;
+let b = 1;
+let c;
+if (index < 1) return "OOPS";
+if (index == 1) return a + b;
+for (let i = 1; i < index; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+}
+return c;
+}
 // Do not edit below this line
 module.exports = fibonacci;
